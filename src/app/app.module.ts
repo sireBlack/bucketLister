b62b05d829187af7d017434fd55ab7be16f0ae37
sireBlack <dblacktin@gmail.com>
2018-06-17 00:59:30 +0000

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataService} from './data.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
@@ -20,8 +21,9 @@ import { AboutComponent } from './about/about.component';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule
+    
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
